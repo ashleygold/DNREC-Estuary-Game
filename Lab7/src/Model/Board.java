b767e6a7cfd835bb7cbc5cdc41362a63b224b9c5
Ashley Gold <angold@udel.edu>
2017-04-04 15:03:47 -0400

    Minor changes

--- Lab7/src/Model/Board.java
+++ Lab7/src/Model/Board.java
@@ -2,6 +2,6 @@ package Model;
 
 public class Board {
 
-	 final public static int frameWidth = 1000;
-	 final public static int frameHeight = 600;
+	 public final static int frameWidth = 500;
+	 public final static int frameHeight = 300;
 }
