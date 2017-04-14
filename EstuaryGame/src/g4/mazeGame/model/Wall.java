f86e6f71697b0b6b3350d595ab36936897c236f2
jwbuxton <jwbuxton@udel.edu>
2017-04-14 19:12:19 -0400

    Collisions detection function in Wall

--- EstuaryGame/src/g4/mazeGame/model/Wall.java
+++ EstuaryGame/src/g4/mazeGame/model/Wall.java
@@ -2,10 +2,21 @@ package g4.mazeGame.model;
 
 public class Wall {
 	
-	int topRight;
-	int bottomLeft;
+	//dimensions
+	final int LEFT;   //x
+	final int RIGHT;  //x
+	final int TOP;    //y
+	final int BOTTOM; //y
 	
-	public Wall(){
+	public Wall(int l, int r, int t, int b){
+		LEFT = l;
+		RIGHT = r;
+		TOP = t;
+		BOTTOM = b;
+	}
+	
+	public boolean checkColl(int x, int y){
+		return (x >= LEFT && x <= RIGHT && y >= TOP && y <= BOTTOM);
 	}
 	
 }
