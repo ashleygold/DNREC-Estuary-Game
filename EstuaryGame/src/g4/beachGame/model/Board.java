46f57db8b1ba81f7f64689e80e422d83d1935592
ziyaoyan <ziyaoyan@udel.edu>
2017-04-17 18:37:15 -0400

    adding code

--- EstuaryGame/src/g4/beachGame/model/Board.java
+++ EstuaryGame/src/g4/beachGame/model/Board.java
@@ -10,6 +10,7 @@ public class Board {
 	ArrayList<Protector> protectorLine;
 	ArrayList<Wave> currWaves;
 	User user;
+	int difficulty;
 	
 	Board(){
 		currWaves = new ArrayList<Wave>();
@@ -17,25 +18,34 @@ public class Board {
 		protectorLine = new ArrayList<Protector>();
 	}
 	
-	public void showWin(){
+	public void win(){
 		
 	}
-	public void showLoss(){
+	
+	public void loss(){
 		
 	}
 	public void restart(){
 		
 	}
+	
+	/*Checks to see if the waves has hit a protector along the shoreline.
+	 *If there is a protector, it's life decreases by 1. If not, level of difficulty 
+	 *drops. */
 	public void checkHitProtector(){
-		Iterator<Wave> wavesit = currWaves.iterator();
-		while (wavesit.hasNext()){
-			Wave currWave = wavesit.next();
+		Iterator<Wave> wavesIt = currWaves.iterator();
+		while (wavesIt.hasNext()){
+			Wave currWave = wavesIt.next();
 			if (currWave.yloc >= SHORELINE){
 				Iterator<Protector> protit = protectorLine.iterator();
 				while (protit.hasNext()){
 					Protector currProt = protit.next();
+					if (currProt==null){
+						difficulty++; 
+						loss(); 
+					}
 					//if either end of the protector is within the bounds of wave, wave has hit it
-					if ((currProt.xloc1 >= currWave.xloc && currProt.xloc1 <= currWave.xloc + currWave.length)||(currProt.xloc2 >= currWave.xloc && currProt.xloc2 <= currWave.xloc + currWave.length))
+					else if ((currProt.xloc1 >= currWave.xloc && currProt.xloc1 <= currWave.xloc + currWave.length)||(currProt.xloc2 >= currWave.xloc && currProt.xloc2 <= currWave.xloc + currWave.length))
 						currProt.loseLife();
 				}
 			}
--- EstuaryGame/src/g4/beachGame/model/Boat.java
+++ EstuaryGame/src/g4/beachGame/model/Boat.java
@@ -1,5 +1,19 @@
 package g4.beachGame.model;
 
-public class Boat {
-
+public abstract class Boat {
+	
+	int xloc; 
+	int yloc; 
+	
+	
+	//getter for X Location of boat
+	public int getXLoc(){
+		return this.xloc;
+	}
+	
+	//getter for Y Location of boat
+	public int getYLoc(){
+		return this.yloc;
+	}
+	
 }
