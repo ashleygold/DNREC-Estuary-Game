6bdf6ce792a1238f4752e91f919254dae68990a3
Jordan Stewart <jstewar@udel.edu>
2017-04-17 09:38:05 -0400

    Changes to MazeView

--- EstuaryGame/src/g4/mazeGame/view/MazeView.java
+++ EstuaryGame/src/g4/mazeGame/view/MazeView.java
@@ -1,5 +1,18 @@
 package g4.mazeGame.view;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import g4.mazeGame.controller.*;
+import g4.mazeGame.model.*;
 
-public class MazeView {
-
+public class MazeView extends JPanel{
+	ArrayList<Wall> walls = new ArrayList<Wall>();
+	
 }
