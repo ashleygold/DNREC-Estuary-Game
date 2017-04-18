92368cbd74bb67622082a4054c9e4af5bcc89f82
Jordan Stewart <jstewar@udel.edu>
2017-04-17 22:37:26 -0400

    Changed BeachView

--- EstuaryGame/src/g4/beachGame/view/BeachView.java
+++ EstuaryGame/src/g4/beachGame/view/BeachView.java
@@ -1,6 +1,14 @@
 package g4.beachGame.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
 import javax.swing.*;
+
+import com.sun.prism.Image;
+
+import g4.mainController.MainMenu;
 
 public class BeachView extends JPanel{
 	private JFrame frame;
@@ -10,10 +18,32 @@ public class BeachView extends JPanel{
 	private final int USER_WIDTH = 100;
 	private final int USER_HEIGHT = 100;
 	
-	private static final String[] crabImages = {"images/StoryImages/TestImage.png",
-	"images/StoryImages/TestImage2.png"};
+	private static final String[] crabImagesLoc = {"images/BeachImages/bluecrab_0.png",
+	"images/BeachImages/bluecrab_1.png", "images/BeachImages/bluecrab_2.png",
+	"images/BeachImages/bluecrab_back.png"};
+	
+	private static final String[] turtleImagesLoc = {"images/BeachImages/bogturtle_left_0.png",
+		"images/BeachImages/bogturtle_left_1.png", "images/BeachImages/bogturtle_left_2.png",
+		"images/BeachImages/bogturtle_right_0.png", "images/BeachImages/bogturtle_right_1.png",
+		"images/BeachImages/bogturtle_right_2.png"};
+	
+	private static final String[] protectorsLoc = {"images/StoryImages/TestImage.png",
+		"images/StoryImages/TestImage2.png", "images/StoryImages/TestImage2.png"};
+	
+	private final ImageIcon[] crabImages = new ImageIcon[crabImagesLoc.length];
+	private final ImageIcon[] protectors = new ImageIcon[protectorsLoc.length];
+	private final ImageIcon[] turtleImages = new ImageIcon[turtleImagesLoc.length];
 	
 	public BeachView(){
+		//loads images into imageIcons
+		for (int i = 0; i < protectors.length; i++)
+			protectors[i] = new ImageIcon(protectorsLoc[i]);
+		for (int i = 0; i < crabImages.length; i++)
+			crabImages[i] = new ImageIcon(crabImagesLoc[i]);
+		for (int i = 0; i < turtleImages.length; i++)
+			turtleImages[i] = new ImageIcon(turtleImagesLoc[i]);
 		
+		frame = new JFrame();
+		frame.getContentPane().add(this);
 	}
 }
