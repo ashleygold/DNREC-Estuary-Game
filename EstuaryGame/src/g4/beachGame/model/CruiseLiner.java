a5c3ce5ca6bce073e88919bf00aed80fb4b01620
ziyaoyan <ziyaoyan@udel.edu>
2017-05-04 22:42:34 -0400

    boats can't spawn on the shore

--- EstuaryGame/src/g4/beachGame/model/CruiseLiner.java
+++ EstuaryGame/src/g4/beachGame/model/CruiseLiner.java
@@ -4,10 +4,11 @@ public class CruiseLiner extends Boat{
 	
 	final int CRUISELINER_SPEED=10;
 	final int BOAT_HEIGHT = 100;
+	final int MIN_DISTANCE_FROM_SHORE = 200;
 	
 	public CruiseLiner(){
 		int randomDir = 1+ (int) (Math.random() * 2);
-		int randomHeight = 100 + (int) (Math.random()*Board.shoreline-100);
+		int randomHeight = 100 + (int) (Math.random()*Board.shoreline-MIN_DISTANCE_FROM_SHORE);
 		if (randomDir==1){
 			this.xloc=0;
 			this.direction=true;
