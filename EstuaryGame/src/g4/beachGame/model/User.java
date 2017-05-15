d1ca81a6d7b05b72226e5fd4763c69d97dae1e6b
Ashley Gold <angold@udel.edu>
2017-05-15 09:10:09 -0400

    beach User javadoc

--- EstuaryGame/src/g4/beachGame/model/User.java
+++ EstuaryGame/src/g4/beachGame/model/User.java
@@ -1,40 +1,71 @@
 package g4.beachGame.model;
 
 public class User {
+	
+	/**height of the crab*/
 	public final static int CRAB_HEIGHT=50;
+	
+	/**width of the crab*/
 	public final static int CRAB_WIDTH=70;
+	
+	/**hit box buffer for collision detection*/
 	private final static int BUFFER = 30;
 	
+	/**starting x location of user*/
 	private final int DEFAULTX = 15;
+	
+	/**starting y location of user*/
 	private final int DEFAULTY = 450;
+	
+	/**x increment changed when user moves*/
 	private final int XINCR = 12;
+	
+	/**y increment changed when user moves*/
 	private final int YINCR = 8;
+	
+	/**x location of user*/
 	private int xLoc;
+	
+	/**y location of user*/
 	private int yLoc;
+	
+	/**picture number of current graphic */
 	private double picNum = 0;
+	
+	/** */
 	int frameCount = 3;
 	
+	/**tells whether the user is in the ocean */
 	public boolean isInOcean = false;
 	
+	/**tells whether the user is on the edge of the screen so they can't walk
+	 * off the screen*/
 	private boolean isOnEdge = false;
 	
+	/**board that the user is on */
 	private final Board board;
 	
+	/**possible directions of user */
 	public final static int STILL = 0, LEFT = 1, RIGHT = 2, UP = 3, DOWN = 4,
 			UP_RIGHT = 5, UP_LEFT = 6, DOWN_RIGHT = 7, DOWN_LEFT = 8;
+	
+	/**direction of user */
 	private int direction = STILL;
 	
 	
+	/**
+	 *Constructor to create a user 
+	 * @param b: the board that the user is on
+	 */
 	public User(Board b){
 		this.board = b;
 		this.xLoc = DEFAULTX;
 		this.yLoc = DEFAULTY;
 	}
 	
-	public int getPicNum(){
-		return (int) picNum;
-	}
-	
+	/**
+	 * Moves the user by changing the direction of the user
+	 */
 	public void move() {
 		//checks nested in the interest of efficiency
 		if(direction!=STILL)
@@ -92,6 +123,10 @@ public class User {
 			isInOcean=false;
 		}
 		isOnEdge = (yLoc >= board.getHeight() - CRAB_HEIGHT - 10);
+	}
+	
+	public int getPicNum(){
+		return (int) picNum;
 	}
 
 	public int getxLoc() {
