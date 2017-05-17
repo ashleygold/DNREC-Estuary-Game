2b8de8f8614446607c28acb538173339d56f2bfe
Jordan Stewart <jstewar@udel.edu>
2017-05-17 16:01:35 -0400

    Turtles

--- EstuaryGame/src/g4/beachGame/model/Turtle.java
+++ EstuaryGame/src/g4/beachGame/model/Turtle.java
@@ -13,7 +13,7 @@ public class Turtle {
 	private final int TURTLE_WIDTH=70;
 	
 	/**Starting x location */
-	private final int DEFAULTX = 2+ (int)(Math.random() * Board.SHORE_WIDTH-50);
+	private final int DEFAULTX = 50+ (int)(Math.random() * Board.SHORE_WIDTH-50);
 	
 	/**Starting y location */
 	private final int DEFAULTY = Board.HEIGHT;
